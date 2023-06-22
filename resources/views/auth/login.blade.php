42ede53702dda2833af2bebba65fcb5897f04a3f
basithbahi <basithbahi@gmail.com>
2023-06-22 18:52:39 +0700

    Update Terbaru Kamis 22/06/2023 18.52

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -37,7 +37,7 @@
               <div class="col-lg-6">
                 <div class="p-5">
                   <div class="text-center">
-                    <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                    <h1 class="h4 text-gray-900 mb-4">Selamat Datang!</h1>
                   </div>
                   <form action="{{ route('login.aksi') }}" method="POST" class="user">
                     @csrf
@@ -59,7 +59,7 @@
                     <div class="form-group">
                       <div class="custom-control custom-checkbox small">
                         <input name="remember" type="checkbox" class="custom-control-input" id="customCheck">
-                        <label class="custom-control-label" for="customCheck">Remember Me</label>
+                        <label class="custom-control-label" for="customCheck">Ingat Saya</label>
                       </div>
                     </div>
                     <button type="submit" class="btn btn-primary btn-block btn-user">Login</button>
